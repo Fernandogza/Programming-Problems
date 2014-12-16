d072591e672221f921c989417ac736337e0dd819
Fernandogza <fernandogarzaconde@gmail.com>
2014-12-16 11:43:51 -0600

    Update 1A.cpp

--- CodeForces/1A.cpp
+++ CodeForces/1A.cpp
@@ -1,2 +1,12 @@
 //Problem 1A Theatre Square
 //http://codeforces.com/problemset/problem/1/A
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main () {
+  int n, m, a;
+  cin >> n >> m >> a;
+  cout << ceil(n/a) * ceil(m/a) << endl;
+}
